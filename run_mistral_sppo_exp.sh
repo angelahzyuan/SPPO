984097fdd2f6a20235587c66d417731a35f0b42e
Sangwoong Yoon <sangwoong24yoon@gmail.com>
2024-09-23 23:17:04 +0000

    fix strange bug in script

--- run_mistral_sppo_exp.sh
+++ run_mistral_sppo_exp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 i=1
-OLD_DATASET_DIR="synthetic_data_reg1_0.01_fixedprompt1-mistral-7b-instruct-sppo-iter1_score"
+OLD_DATASET_DIR="synthetic_data_sft_fixedprompt1-mistral-7b-instruct-sppo-iter1_score"
 MODEL="mistralai/Mistral-7B-Instruct-v0.2"
 PROMPT="UCLA-AGI/data-mistral-7b-instruct-sppo-iter1"
 
@@ -9,9 +9,9 @@ exp_list=(sppo_forward_importance 0.01
           sppo_reversekl 0.1
           sppo_reversekl 0.01)
 
-for ((i=0; i<${#exp_list[@]}; i+=2)); do
-    LOSS_TYPE=${exp_list[i]}
-    REG_COEF=${exp_list[i+1]}
+for ((k=0; k<${#exp_list[@]}; k+=2)); do
+    LOSS_TYPE=${exp_list[k]}
+    REG_COEF=${exp_list[k+1]}
     OUTPUT_DIR="checkpoints/${LOSS_TYPE}-${REG_COEF}-PromptA-Mistral-7B-Instruct-SPPO-Iter${i}"
     OUT="data-${LOSS_TYPE}-${REG_COEF}-promptA-mistral-7b-instruct-sppo-iter${i}"
     DATASET_DIR="synthetic_data_${LOSS_TYPE}_${REG_COEF}_promptA-mistral-7b-instruct-sppo-iter${i}_score"
@@ -25,15 +25,3 @@ for ((i=0; i<${#exp_list[@]}; i+=2)); do
 
 done
     
-
-
-
-# OUTPUT_DIR="checkpoints/${LOSS_TYPE}-${REG_COEF}-PromptA-Mistral-7B-Instruct-SPPO-Iter${i}"
-# PROMPT="UCLA-AGI/data-mistral-7b-instruct-sppo-iter1"
-# OUT="data-${LOSS_TYPE}-${REG_COEF}-promptA-mistral-7b-instruct-sppo-iter${i}"
-# DATASET_DIR="synthetic_data_${LOSS_TYPE}_${REG_COEF}_promptA-mistral-7b-instruct-sppo-iter${i}_score"
-# 
-# # bash scripts/generate.sh --model $MODEL --prompt $PROMPT --out_path $OUT
-# bash scripts/pipeline_reg.sh --model $MODEL --iter $i \
-# --dataset $DATASET_DIR \
-# --output_dir $OUTPUT_DIR --num 1 --loss_type ${LOSS_TYPE} --reg_coef ${REG_COEF}
--- run_sppo_mistral_fixedprompt_reg.sh
+++ run_sppo_mistral_fixedprompt_reg.sh
@@ -7,12 +7,12 @@ for i in $(seq 1 $iter_num); do
     else
         MODEL="checkpoints/FixedPrompt1-Mistral-7B-Instruct-SPPO-Iter$((i-1))"
     fi
-    OUTPUT_DIR="checkpoints/Entropy0.1-FixedPrompt1-Mistral-7B-Instruct-SPPO-Iter${i}"
+    OUTPUT_DIR="checkpoints/ForwardImportance0.01-FixedPrompt1-Mistral-7B-Instruct-SPPO-Iter${i}"
     PROMPT="UCLA-AGI/data-mistral-7b-instruct-sppo-iter1"
-    OUT="data-entropy0.1-fixedprompt1-mistral-7b-instruct-sppo-iter${i}"
+    OUT="data-forwardimportance0.01-fixedprompt1-mistral-7b-instruct-sppo-iter${i}"
 
-    bash scripts/generate.sh --model $MODEL --prompt $PROMPT --out_path $OUT
+    # bash scripts/generate.sh --model $MODEL --prompt $PROMPT --out_path $OUT
     bash scripts/pipeline_reg.sh --model $MODEL --iter $i \
-    --dataset "synthetic_data_entropy0.1_fixedprompt1-mistral-7b-instruct-sppo-iter${i}_score" \
-    --output_dir $OUTPUT_DIR --num 1 --loss_type "sppo_entropy" --reg_coef 0.1
+    --dataset "synthetic_data_forwardimportance0.01_fixedprompt1-mistral-7b-instruct-sppo-iter${i}_score" \
+    --output_dir $OUTPUT_DIR --num 1 --loss_type "sppo_forward_importance" --reg_coef 0.01
 done
